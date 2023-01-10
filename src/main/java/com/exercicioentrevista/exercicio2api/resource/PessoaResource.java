b7e8faf2d488db4aabb4bdf8eb5057fc60cfb102
Victor Augusto Alves de Carvalho <105305131+VictorAlves94@users.noreply.github.com>
2023-01-10 18:11:38 -0300

    tudo funcionando,falta criar endereço no DB

--- src/main/java/com/exercicioentrevista/exercicio2api/resource/PessoaResource.java
+++ src/main/java/com/exercicioentrevista/exercicio2api/resource/PessoaResource.java
@@ -13,15 +13,31 @@ public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    public PessoaResource(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
     @GetMapping
     public List<Pessoa> listar(){
         return pessoaRepository.findAll();
     }
-//    @PostMapping
-//    public void cadastrarPessoa(RequestBody) {
-//
-//        pessoaRepository.save(Entity);
-//
-//    }
+    @PostMapping
+    public Pessoa criar(@RequestBody Pessoa pessoa) {
+
+        return pessoaRepository.save(pessoa);
+    }
+    @PutMapping
+    public Pessoa atualizar(@RequestBody Pessoa pessoa) {
+        if (pessoa.getId() > 0) {
+            return pessoaRepository.save(pessoa);
+        }
+        return pessoa;
+    }
+    @DeleteMapping
+    public void deletar(@RequestBody Pessoa pessoa){
+        pessoaRepository.delete(pessoa);
 
     }
+
+    }
+
