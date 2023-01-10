507b212c71472116821978867cd8b59788ea3ee5
Victor Augusto Alves de Carvalho <105305131+VictorAlves94@users.noreply.github.com>
2023-01-10 15:04:40 -0300

    agora vai

---
+++ src/test/java/com/exercicioentrevista/exercicio2api/Exercicio2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicioentrevista.exercicio2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
