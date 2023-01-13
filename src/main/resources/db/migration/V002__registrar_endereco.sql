2ba94e0061fa6eb1dc26e28ac2cc092e29297c14
Victor Augusto Alves de Carvalho <105305131+VictorAlves94@users.noreply.github.com>
2023-01-13 18:43:26 -0300

    problemas para fazer interação com o Endereco

--- src/main/resources/db/migration/V002__registrar_endereco.sql
+++ src/main/resources/db/migration/V002__registrar_endereco.sql
@@ -1,4 +1,4 @@
-CREATE TABLE(
+CREATE TABLE endereco(
 endereco_id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 logadouro VARCHAR(50)NOT NULL,
 cep VARCHAR(20) NOT NULL,
@@ -7,4 +7,4 @@ cidade VARCHAR(50)NOT NULL,
 pessoa_id BIGINT(20)                                                                                                                         NOT NULL,
 FOREIGN KEY (endereco_id) REFERENCES endereco(endereco_id),
 FOREIGN KEY (pessoa_id) REFERENCES pessoa(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
